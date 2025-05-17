d6b48a9569d9823903ddfaf4d51deb51dc765d4a
Asaph Velazquez <saulvelpa120@gmail.com>
2025-05-16 22:18:31 -0600

    Update SQL

---
+++ SQL/SQL.sql
@@ -0,0 +1,30 @@
+Create table Users(
+	userId serial primary key,
+	
+	userName varchar(50) unique,
+	userEmail varchar(50) unique,
+	userPassword varchar(50)
+);
+
+create table Tasks(
+	taskID serial primary key,
+	userId int not null,
+	
+	taskName varchar(50),
+	taskDescription text,
+	taskDate date,
+	taskPriority varchar(50),
+	taskStatus varchar(50),
+	taskCategory varchar(50),
+	taskAttachments text,
+
+	foreign key (userId) references Users(userId)
+);
+
+create table Administrators(
+	adminId serial primary key,
+	
+	adminName varchar(50) unique,
+	adminEmail varchar(50) unique,
+	adminPassword varchar(50)	
+);
