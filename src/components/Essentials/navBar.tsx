d543e0dce95ee6a2166ae833de6074e54ade1513
Asaph Velazquez <saulvelpa120@gmail.com>
2025-05-17 23:24:03 -0600

    updated nav icons

--- src/components/Essentials/navBar.tsx
+++ src/components/Essentials/navBar.tsx
@@ -1,78 +1,131 @@
 function NavBar() {
   return (
-  <div className="container mt-4">
-    <nav className="navbar navbar-expand-lg shadow rounded" style={{marginBottom: "5dvh"}}>
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="40" fill="currentColor" className="bi bi-textarea-t" viewBox="0 0 16 16">
-          <path d="M1.5 2.5A1.5 1.5 0 0 1 3 1h10a1.5 1.5 0 0 1 1.5 1.5v3.563a2 2 0 0 1 0 3.874V13.5A1.5 1.5 0 0 1 13 15H3a1.5 1.5 0 0 1-1.5-1.5V9.937a2 2 0 0 1 0-3.874zm1 3.563a2 2 0 0 1 0 3.874V13.5a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V9.937a2 2 0 0 1 0-3.874V2.5A.5.5 0 0 0 13 2H3a.5.5 0 0 0-.5.5zM2 7a1 1 0 1 0 0 2 1 1 0 0 0 0-2m12 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
-          <path d="M11.434 4H4.566L4.5 5.994h.386c.21-1.252.612-1.446 2.173-1.495l.343-.011v6.343c0 .537-.116.665-1.049.748V12h3.294v-.421c-.938-.083-1.054-.21-1.054-.748V4.488l.348.01c1.56.05 1.963.244 2.173 1.496h.386z"/>
-        </svg>
+    <div className="container mt-4">
+      <nav
+        className="navbar navbar-expand-lg shadow rounded"
+        style={{ marginBottom: "5dvh" }}
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="40"
+              fill="currentColor"
+              className="bi bi-textarea-t"
+              viewBox="0 0 16 16"
+            >
+              <path d="M1.5 2.5A1.5 1.5 0 0 1 3 1h10a1.5 1.5 0 0 1 1.5 1.5v3.563a2 2 0 0 1 0 3.874V13.5A1.5 1.5 0 0 1 13 15H3a1.5 1.5 0 0 1-1.5-1.5V9.937a2 2 0 0 1 0-3.874zm1 3.563a2 2 0 0 1 0 3.874V13.5a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V9.937a2 2 0 0 1 0-3.874V2.5A.5.5 0 0 0 13 2H3a.5.5 0 0 0-.5.5zM2 7a1 1 0 1 0 0 2 1 1 0 0 0 0-2m12 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2" />
+              <path d="M11.434 4H4.566L4.5 5.994h.386c.21-1.252.612-1.446 2.173-1.495l.343-.011v6.343c0 .537-.116.665-1.049.748V12h3.294v-.421c-.938-.083-1.054-.21-1.054-.748V4.488l.348.01c1.56.05 1.963.244 2.173 1.496h.386z" />
+            </svg>
+          </a>
+          <button
+            style={{ color: "var(--text-color)" }}
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          />
 
-        </a>
-        <button
-          style={{color: "var(--text-color)"}}
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        />
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link d-flex align-items-center" aria-current="page" href="/">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="bi bi-house-door-fill me-2"
-                  viewBox="0 0 16 16"
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a
+                  className="nav-link d-flex align-items-center"
+                  aria-current="page"
+                  href="/"
                 >
-                  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
-                </svg>
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link d-flex align-items-center" href="/rForm">
-                <i className="bi bi-person-plus-fill me-2"></i>
-                Register
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link d-flex align-items-center" href="/Login">
-                <i className="bi bi-person-plus-fill me-2"></i>
-                Login
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link d-flex align-items-center" href="/ToDoForm">
-                <i className="bi bi-check2-square me-2"></i>
-                Create New Pending Task
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link d-flex align-items-center" href="/ToDoList">
-                <i className="bi bi-check2-square me-2"></i>
-                Pending Tasks List
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link d-flex align-items-center" href="/DbTest">
-                <i className="bi bi-database me-2"></i>
-                Database Test
-              </a>
-            </li>
-          </ul>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-house-door-fill me-2"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5" />
+                  </svg>
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link d-flex align-items-center" href="/rForm">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-person-add"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+                    <path d="M8.256 14a4.5 4.5 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10q.39 0 .74.025c.226-.341.496-.65.804-.918Q8.844 9.002 8 9c-5 0-6 3-6 4s1 1 1 1z" />
+                  </svg>
+                  Register
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link d-flex align-items-center" href="/Login">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-person-plus-fill me-2"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+                  </svg>
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link d-flex align-items-center"
+                  href="/ToDoForm"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-bookmark-plus-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5m6.5-11a.5.5 0 0 0-1 0V6H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V7H10a.5.5 0 0 0 0-1H8.5z"
+                    />
+                  </svg>
+                  Create New Pending Task
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link d-flex align-items-center"
+                  href="/ToDoList"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-bookmark"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
+                  </svg>
+                  Pending Tasks List
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </div>
+      </nav>
+    </div>
   );
 }
 export default NavBar;
