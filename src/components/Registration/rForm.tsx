6110d4e7890b9736f6f53a645e734ee8a0d4f8b5
Asaph Velazquez <saulvelpa120@gmail.com>
2025-05-20 22:16:58 -0600

    rForm validation progress

--- src/components/Registration/rForm.tsx
+++ src/components/Registration/rForm.tsx
@@ -1,9 +1,62 @@
+import { useState, useEffect, useRef} from "react";
+import axios from "axios"
+
 function rForm() {
+  {/*Getting values from the form*/ }
+  const userNameInfo  = useRef<HTMLInputElement>(null);
+  const passwordInfo = useRef<HTMLInputElement>(null);
+  const emailInfo = useRef<HTMLInputElement>(null);
+  const firstNameInfo = useRef<HTMLInputElement>(null);
+  const lastNameInfo = useRef<HTMLInputElement>(null);
+  const agreeInfo = document.getElementById("agree")
+
+  {/* Validations*/}
+  const Regex = {
+    username: /^[a-zA-Z0-9]{3,20}$/,
+    password: /^[a-zA-Z0-9]{8,20}$/,
+    email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    firstName: /^[a-zA-Z]{3,20}$/,
+    lastName: /^[a-zA-Z]{3,20}$/
+  };
+
+  const handleSubmit = () => {
+    const username = userNameInfo.current?.value || '';
+    const password = passwordInfo.current?.value || '';
+    const email = emailInfo.current?.value || '';
+    const firstName = firstNameInfo.current?.value || '';
+    const lastName = lastNameInfo.current?.value || '';
+
+    if (!Regex.username.test(username)) {
+      alert("Username is not valid");
+      return;
+    }
+    if (!Regex.password.test(password)) {
+      alert("Password is not valid");
+      return;
+    }
+    if (!Regex.email.test(email)) {
+      alert("Email is not valid");
+      return;
+    }
+    if (!Regex.firstName.test(firstName)) {
+      alert("First name is not valid");
+      return;
+    }
+    if (!Regex.lastName.test(lastName)) {
+      alert("Last name is not valid");
+      return;
+    }
+    
+  }
+
+  {/*Fetching users from the database*/}
+  useEffect(()=>{
+    axios.get("/api/users")
+  })
+
   return (
     <div className="container center">
-
       <form className="row g-3">
-
          <div className="col-md-12">
           <label htmlFor="validationServerUsername" className="form-form-label d-flex justify-content-center">
             Username
@@ -18,9 +71,8 @@ function rForm() {
             />
             <div
               id="validationServerUsernameFeedback"
-              className="invalid-feedback"
+              className="feedback"
             >
-              Please choose a username.
             </div>
           </div>
         </div>
@@ -32,12 +84,13 @@ function rForm() {
             First name
           </label>
           <input
+            ref={firstNameInfo}
             type="text"
-            className="form-control is-valid"
+            className="form-control"
             id="validationServer01"
             required
           />
-          <div className="valid-feedback">Looks good!</div>
+          <div className="feedback"></div>
         </div>
 
         <div className="col-md-6">
@@ -50,7 +103,7 @@ function rForm() {
             id="validationServer02"
             required
           />
-          <div className="valid-feedback">Looks good!</div>
+          <div className="feedback"></div>
         </div>
 
        
@@ -72,9 +125,8 @@ function rForm() {
             </span>
             <div
               id="validationServerUsernameFeedback"
-              className="invalid-feedback"
+              className="feedback"
             >
-              Please choose a username.
             </div>
           </div>
         </div>
--- src/components/ToDo/ToDoForm.tsx
+++ src/components/ToDo/ToDoForm.tsx
@@ -1,31 +1,76 @@
+import { useRef, useEffect, useState  } from "react";
+import axios from "axios"
+
 function ToDohtmlForm() {
+  const taskNameInfo = useRef<HTMLInputElement>(null);
+  const descriptionInfo = useRef<HTMLTextAreaElement>(null);
+  const taskDateInfo = useRef<HTMLInputElement>(null);
+  const taskPriorityInfo = useRef<HTMLSelectElement>(null);
+  const taskStatusInfo = useRef<HTMLSelectElement>(null);
+  const taskCategoryInfo = useRef<HTMLInputElement>(null);
+  const taskAttachmentsInfo = useRef<HTMLInputElement>(null);
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/tasks")
+      .then(response => setTasks(response.data))
+      .catch(error => console.error("Error fetching tasks:", error));
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const taskData = {
+      userId: 1,
+      taskName: taskNameInfo?.current?.value,
+      taskDescription: descriptionInfo?.current?.value,
+      taskDate: taskDateInfo?.current?.value,
+      taskPriority: taskPriorityInfo?.current?.value,
+      taskStatus: taskStatusInfo?.current?.value,
+      taskCategory: taskCategoryInfo?.current?.value,
+      taskAttachments: taskAttachmentsInfo?.current?.value,
+    }
+    console.log(taskData);
+
+    axios.post("/api/tasks", taskData)
+      .then(response => {
+        console.log("Task created successfully:", response.data);
+        axios.get("/api/tasks")
+          .then(response => setTasks(response.data))
+          .catch(error => console.error("Error fetching tasks:", error));
+      })
+      .catch(error => {
+        console.error("Error creating task:", error);
+      });
+  }
+
   return (
     <main className="d-flex justify-content-center align-items-center" style={{textAlign: "center"}}>
       <br />
-      <form className="container">
+      <form className="container" onSubmit={handleSubmit}>
         <div className="row">
           {/* Task Name */}
           <div className="col-md-4 mb-3">
             <label htmlFor="FormTaskName" className="form-label">Task Name</label>
-            <input type="text" className="form-control" id="FormTaskName" required />
+            <input type="text" className="form-control" id="FormTaskName" ref={taskNameInfo} required />
           </div>
  
           {/* Description */}
           <div className="col-md-4 mb-3">
-            <label htmlFor="FormDescription" className="form-label">Description (Optional)</label>
-            <textarea className="form-control" id="FormDescription" rows={3}></textarea>
+            <label htmlFor="FormDescription" className="form-label">Description</label>
+            <textarea className="form-control" id="FormDescription" ref={descriptionInfo} rows={3} required></textarea>
           </div>
 
           {/* Due Date */}
           <div className="col-md-4 mb-3">
             <label htmlFor="FormDate" className="form-label">Due Date</label>
-            <input type="date" className="form-control" id="FormDate" required />
+            <input type="date" className="form-control" id="FormDate" ref={taskDateInfo} required />
           </div>
 
           {/* Priority */}
           <div className="col-md-4 mb-3">
             <label htmlFor="FormPriority" className="form-label">Priority</label>
-            <select className="form-control" id="FormPriority" required>
+            <select className="form-control" id="FormPriority" ref={taskPriorityInfo} required>
               <option value="">Select priority</option>
               <option value="Alta">Alta</option>
               <option value="Media">Media</option>
@@ -36,7 +81,7 @@ function ToDohtmlForm() {
           {/* Status */}
           <div className="col-md-4 mb-3">
             <label htmlFor="FormStatus" className="form-label">Status</label>
-            <select className="form-control" id="FormStatus" required>
+            <select className="form-control" id="FormStatus" ref={taskStatusInfo} required>
               <option value="">Select status</option>
               <option value="Pendiente">Pendiente</option>
               <option value="En progreso">En progreso</option>
@@ -46,15 +91,15 @@ function ToDohtmlForm() {
 
           {/* Category */}
           <div className="col-md-4 mb-3">
-            <label htmlFor="FormCategory" className="form-label">Category (Optional)</label>
-            <input type="text" className="form-control" id="FormCategory" placeholder="Trabajo, Personal, Estudios..." />
+            <label htmlFor="FormCategory" className="form-label">Category</label>
+            <input type="text" className="form-control" id="FormCategory" ref={taskCategoryInfo} placeholder="Trabajo, Personal, Estudios..." />
           </div>
 
           <div className="col-md-4 mb-3"></div>
           {/* Attachments */}
           <div className="col-md-9 mb-3" style={{ marginLeft: "auto", marginRight: "auto" }}>
-            <label htmlFor="FormAttachments" className="form-label d-flex justify-content-center">Attachments (Optional)</label>
-            <input type="file" className="form-control" id="FormAttachments" multiple />
+            <label htmlFor="FormAttachments" className="form-label d-flex justify-content-center">Attachments</label>
+            <input type="file" className="form-control" id="FormAttachments" ref={taskAttachmentsInfo} multiple />
           </div>
         </div>
 
